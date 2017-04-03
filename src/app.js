a0656afbcbb5693e4b1de43f7654086fd8e10d3c
langdonx <langdon@gmail.com>
2017-04-03 13:55:02 -0400

    finished renaming stateConfig ;)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import routeConfig from './routeConfig';
+import stateConfig from './stateConfig';
 import services from './services'; // eslint-disable-line no-unused-vars
 
 angular.module('app', ['app.services', 'ui.router', 'oc.lazyLoad'])
     .value('AppVersion', document.querySelector('base').getAttribute('version'))
-    .config(routeConfig);
+    .config(stateConfig);
